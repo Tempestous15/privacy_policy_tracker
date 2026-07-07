5c035b98b8d0c6dcf59a372cdf0033376d3c6094
Tempestous15 <ethandanger06@gmail.com>
2026-07-07 09:42:47 -0400

    Added login/dashboard and improved theme

--- website/tracker/views.py
+++ website/tracker/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import Website
 
@@ -14,6 +15,7 @@ def privacy(request):
     return render(request, "tracker/privacy.html")
 
 
+@login_required
 def dashboard(request):
     websites = Website.objects.all()[:20]
     return render(request, "tracker/dashboard.html", {"websites": websites})
